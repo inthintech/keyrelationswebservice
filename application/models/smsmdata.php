2ad360dfb6ffe38d50761008a5018bff57aa354c
keyrelations <keyrelations@gmail.com>
2016-02-29 00:40:09 +0530

    change

--- application/models/smsmdata.php
+++ application/models/smsmdata.php
@@ -51,9 +51,9 @@ Class Smsmdata extends CI_Model
 		
 	}
 	
-	public function updateMovieData($movieId,$name,$img,$backimg,$year)
+	public function updateMovieData($movieId,$name,$img,$year)
     {
-		$query = $this->db->query("insert into smsm_movie(tmdb_movie_id,movie_name,movie_poster_image,movie_backdrop_image,release_year) values(".$movieId.",'".$name."','".$img."','".$backimg."','".$year."')");
+		$query = $this->db->query("insert into smsm_movie(tmdb_movie_id,movie_name,movie_poster_image,release_year) values(".$movieId.",'".$name."','".$img."','".$year."')");
 	}
 	
 	public function updateMovieGenreData($movieId,$genreId)
