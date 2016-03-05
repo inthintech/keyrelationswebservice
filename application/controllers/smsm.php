48de2af8a8d33b916ed59a32031600a14243c5eb
keyrelations <keyrelations@gmail.com>
2016-03-05 18:41:10 +0530

    change

--- application/controllers/smsm.php
+++ application/controllers/smsm.php
@@ -423,7 +423,7 @@ class Smsm extends CI_Controller {
 			
 			// continue the module only if user is authenticated
 			
-			$result = $this->smsmdata->updateUserMovieSuggestion($movieId,$userId,$suggId);
+			//$result = $this->smsmdata->updateUserMovieSuggestion($movieId,$userId,$suggId);
 			
 			/*
 			if($result){
