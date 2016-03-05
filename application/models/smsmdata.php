2e1421b651b955cad89ee088d5a4aaccd012ee2b
keyrelations <keyrelations@gmail.com>
2016-03-05 19:17:44 +0530

    change

--- application/models/smsmdata.php
+++ application/models/smsmdata.php
@@ -164,7 +164,7 @@ order by a.crte_ts desc");
 		
 		if($suggId==1){
 			//suggest movie
-			$updtquery = $this->db->query("select movie_id from smsm_movie where tmdb_movie_id=".$movieId);
+			$updtquery = $this->db->query("update smsm_movieuser set is_suggested_f='Y' where user_id=1 and movie_id=2");
 		}
 		else{
 			//unsuggest movie
