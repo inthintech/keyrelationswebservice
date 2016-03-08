acadf0be74a6404443149b295c63c95f5e269c3a
keyrelations <keyrelations@gmail.com>
2016-03-08 17:28:55 +0530

    change

--- application/controllers/smsm.php
+++ application/controllers/smsm.php
@@ -642,9 +642,10 @@ class Smsm extends CI_Controller {
 	}
 	
 	public function getIMDBRating($imdbID){
-		/*
+		
 		//$html = file_get_contents('http://www.imdb.com/title/tt1431045/');
 		$html=file_get_html('http://www.imdb.com/title/'.$imdbID.'/');
+		/*
 		$rating=null;
 		if($html){
 		foreach($html->find('span[itemprop="ratingValue"]') as $e){
